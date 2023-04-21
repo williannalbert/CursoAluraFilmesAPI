f61f5a1c92ada0f93452ad7f2777032f347910fe
WillianNalbert <williannalbert@hotmail.com>
2023-04-21 20:17:43 -0300

    Inclusão do projeto no git

---
+++ CursoAluraFilmesAPI/CursoAluraFilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,20 @@
+﻿using CursoAluraFilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoAluraFilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody]Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+        Console.WriteLine(filme.Genero);
+    }
+}
