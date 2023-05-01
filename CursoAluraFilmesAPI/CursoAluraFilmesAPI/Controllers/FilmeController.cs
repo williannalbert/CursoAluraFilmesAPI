96915f36ab1cb64d10911e7a9fc462cde3f7421d
WillianNalbert <williannalbert@hotmail.com>
2023-05-01 11:41:34 -0300

    Criação das rotas de criação, retorno e retorno unico

--- CursoAluraFilmesAPI/CursoAluraFilmesAPI/Controllers/FilmeController.cs
+++ CursoAluraFilmesAPI/CursoAluraFilmesAPI/Controllers/FilmeController.cs
@@ -8,14 +8,28 @@ namespace CursoAluraFilmesAPI.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 1;
 
     [HttpPost]
-    public void AdicionaFilme([FromBody]Filme filme)
+    public IActionResult AdicionaFilme([FromBody]Filme filme)
     {
-
+        filme.Id = id++; 
         filmes.Add(filme);
-        Console.WriteLine(filme.Titulo);
-        Console.WriteLine(filme.Genero);
-        Console.WriteLine(filme.Duracao);
+        return CreatedAtAction(nameof(RetornaFilme), new { id = filme.Id }, filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RetornaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 10) 
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RetornaFilme(int id)
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if(filme == null)
+            return NotFound();
+        return Ok(filme);
     }
 }
--- CursoAluraFilmesAPI/CursoAluraFilmesAPI/Models/Filme.cs
+++ CursoAluraFilmesAPI/CursoAluraFilmesAPI/Models/Filme.cs
@@ -4,11 +4,15 @@ namespace CursoAluraFilmesAPI.Models;
 
 public class Filme
 {
+    public int Id { get; set; }
+
     [Required(ErrorMessage = "O título do filme é obrigatório")]
     public string Titulo { get; set; }
+
     [Required(ErrorMessage = "O genero do filme é obrigatório")]
     [MaxLength(50, ErrorMessage = "O tamanho máximo do gênero é 50 caracteres")]
     public string Genero { get; set; }
+
     [Required]
     [Range(70, 600, ErrorMessage = "Duração deve ter entre 70 e 600 min")]
     public int Duracao { get; set; }
